deb70b070c4ccde752d616f46607274d906c4acc
Iskier2 <iskrafilip12@interia.pl>
2024-04-15 19:39:05 +0200

    serial connection

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "pico/stdlib.h"
 #include "pico/cyw43_arch.h"
 
@@ -9,8 +10,10 @@ int main() {
     }
     while (true) {
         cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
-        sleep_ms(1250);
+        printf("LED ON\n");
+        sleep_ms(1000);
         cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 0);
-        sleep_ms(250);
+        printf("LED OFF\n");
+        sleep_ms(1000);
     }
 }
