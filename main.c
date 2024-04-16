4a88959e3402fe96fe88ab0c16d99e401685f0c9
Iskier2 <iskrafilip12@interia.pl>
2024-04-16 13:01:07 +0200

    web server

--- main.c
+++ main.c
@@ -1,19 +1,22 @@
-#include <stdio.h>
+
+#include "lwip/apps/httpd.h"
 #include "pico/stdlib.h"
 #include "pico/cyw43_arch.h"
-
+#include "lwipopts.h"
+#include "cgi.h"
+const char wifi_name[] = "";
+const char wifi_pass[] = "";
 int main() {
     stdio_init_all();
-    if (cyw43_arch_init()) {
-        printf("Wi-Fi init failed");
-        return -1;
+    cyw43_arch_init();
+    cyw43_arch_enable_sta_mode();
+    while(cyw43_arch_wifi_connect_timeout_ms(wifi_name, wifi_pass, CYW43_AUTH_WPA2_AES_PSK, 30000)!=0){
+        printf("connecting...\n");
     }
-    while (true) {
-        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
-        printf("LED ON\n");
-        sleep_ms(1000);
-        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 0);
-        printf("LED OFF\n");
-        sleep_ms(1000);
-    }
+    printf("connected!");
+    httpd_init();
+    cgi_init();
+    while(1){
+        printf("XD");
+    };
 }
